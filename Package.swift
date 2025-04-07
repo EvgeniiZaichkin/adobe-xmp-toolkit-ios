b0cd370fd1db6ffcb1a62ea105fac5e885fe38ec
Evgenii Zaichkin <ezaichkin@onevizion.com>
2025-04-07 14:56:39 +0500

    Remove AdobeXmpToolkit.xcframework.zip, update paths in Package.swift to 1.0.5 version.

--- Package.swift
+++ Package.swift
@@ -16,12 +16,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "XMPCore",
-            url: "https://github.com/EvgeniiZaichkin/adobe-xmp-toolkit-ios/releases/download/1.0.4/XMPCore.xcframework.zip",
+            url: "https://github.com/EvgeniiZaichkin/adobe-xmp-toolkit-ios/releases/download/1.0.5/XMPCore.xcframework.zip",
             checksum: "9c849ba6b3fa14dd9522ea94a4ccc09524bb88786fa8fe76f1665bce1061a6c0"
         ),
         .binaryTarget(
             name: "XMPFiles",
-            url: "https://github.com/EvgeniiZaichkin/adobe-xmp-toolkit-ios/releases/download/1.0.4/XMPFiles.xcframework.zip",
+            url: "https://github.com/EvgeniiZaichkin/adobe-xmp-toolkit-ios/releases/download/1.0.5/XMPFiles.xcframework.zip",
             checksum: "30b7f7aa2a6e69f7574e6a540b22e6b9304b22b202b073454c6ad5510d6895b0"
         )
     ]
