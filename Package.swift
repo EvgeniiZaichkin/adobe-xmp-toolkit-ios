201a9c729b23697e499801eaa233205a43b3e312
Evgenii Zaichkin <ezaichkin@onevizion.com>
2025-04-08 18:14:42 +0500

    Add headers to .xcframework files. Update checksums and versions.

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "XMPCore",
-            url: "https://github.com/EvgeniiZaichkin/adobe-xmp-toolkit-ios/releases/download/1.0.5/XMPCore.xcframework.zip",
-            checksum: "9c849ba6b3fa14dd9522ea94a4ccc09524bb88786fa8fe76f1665bce1061a6c0"
+            url: "https://github.com/EvgeniiZaichkin/adobe-xmp-toolkit-ios/releases/download/1.0.6/XMPCore.xcframework.zip",
+            checksum: "3115b2e38923748a7ec17173a1ca26deb6e12c082e78b77c9f7527b3ba3dda5f"
         ),
         .binaryTarget(
             name: "XMPFiles",
-            url: "https://github.com/EvgeniiZaichkin/adobe-xmp-toolkit-ios/releases/download/1.0.5/XMPFiles.xcframework.zip",
-            checksum: "30b7f7aa2a6e69f7574e6a540b22e6b9304b22b202b073454c6ad5510d6895b0"
+            url: "https://github.com/EvgeniiZaichkin/adobe-xmp-toolkit-ios/releases/download/1.0.6/XMPFiles.xcframework.zip",
+            checksum: "b9e991e3b5e05a8d1f06a69f1928a599b9c7737814679671dabbd1970f9f9a7e"
         )
     ]
 )
